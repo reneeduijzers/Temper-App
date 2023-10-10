578494c39e325313eea98d06efe7e0e7b5a0e4a6
Renee Duijzers <>
2023-10-10 10:11:23 +0200

    remove "

--- src/components/Actions/ActionItem.tsx
+++ src/components/Actions/ActionItem.tsx
@@ -15,7 +15,7 @@ export default function ActionItem({
                     "absolute right-0 bg-temper-green hover:bg-transparent hover:text-temper-green hover:border hover:border-temper-green hover:scale-110 text-neutral-900 font-semibold py-2 px-4 rounded m-3"
                 }
             >
-                "Time travel
+                Time travel
             </button>
         </div>
     );
